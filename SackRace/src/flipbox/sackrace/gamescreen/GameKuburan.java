a2452382b290a63403ea5c983b3ab5958120a5dc
QED <QED@QED-PC>
2012-08-07 13:02:01 +0700

    tambah player data

---
+++ SackRace/src/flipbox/sackrace/gamescreen/GameKuburan.java
@@ -0,0 +1,136 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flipbox.sackrace.gamescreen;
+
+import flipbox.sackrace.game.GameMidlet;
+import flipbox.sackrace.game.IGameScene;
+import flipbox.sackrace.level.LevelGenerator;
+import flipbox.sackrace.nongamescreen.MapScene;
+import flipbox.sackrace.object.Player;
+import flipbox.sackrace.staticvalue.StaticData;
+import flipbox.sackrace.staticvalue.TypeList;
+import flipbox.sackrace.ui.AnimatedSprite;
+import flipbox.sackrace.ui.ButtonImageItem;
+import flipbox.sackrace.ui.ImageItem;
+import java.io.IOException;
+import javax.microedition.lcdui.Graphics;
+import javax.microedition.lcdui.Image;
+
+/**
+ *
+ * @author Yulistiyan Wardhana
+ */
+public class GameKuburan implements IGameScene {
+
+    private Player player;
+    ImageItem backgroundImage;
+    ButtonImageItem buttonSlide;
+    //AnimatedSprite sprite;
+    boolean start;
+    private boolean hasInit;
+    private static int BACKGROUND_POSA;
+    private static int BACKGROUND_POSB;
+    boolean soundOn;
+    private GameMidlet midlet;
+    private static long timeLapsed = 0;
+    private boolean hasRenderBackground;
+
+    public void setGameMidlet(GameMidlet midelet) {
+        midlet = midelet;
+    }
+
+    private void initObstacles() {
+        LevelGenerator.initConstraints(6, 10, 0, 0, 1, 2, 30, 50);
+        LevelGenerator.initDistance(100, 300, 180, 200);
+        LevelGenerator.initObjective(TypeList.DISTANCE, 1000);
+        LevelGenerator.generateObstacles();
+        LevelGenerator.generateCoins();
+    }
+
+    private void initPlayer() {
+        try {
+            player = new Player();
+            Image bagong = StaticData.rotateImage(Image.createImage("/resource/chars/bagong_lompat.png"), 90);
+            player.setSprite(new AnimatedSprite(bagong, 76, 55, 3));
+            player.setJumpHeight(10);
+            player.setName("Bagong");
+            player.setBloodLevel(5);
+            player.getSprite().setPosition(17, 50);
+            player.getSprite().play();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void initBackground() throws IOException {
+        BACKGROUND_POSA = 0;
+        BACKGROUND_POSB = backgroundImage.getWidth() - 40;
+        backgroundImage = new ImageItem("/resource/grave/Sack Runner-03.jpg");
+    }
+
+    public void initResource() throws IOException {
+        initPlayer();
+        initBackground();
+        initObstacles();
+        hasInit = true;
+        start = true;
+    }
+
+    boolean finish;
+    
+    public void render(Graphics g) {
+        if (!hasInit) {
+            return;
+        }
+        if (!hasRenderBackground) {
+            renderBackground(3, g);
+        }
+        if (start) {
+            //clear(g);
+            finish = LevelGenerator.run(g);
+            System.out.println(backgroundImage.getWidth());
+            player.getSprite().update(timeLapsed);
+            timeLapsed++;
+            player.getSprite().paint(g);
+            hasRenderBackground = false;
+        }
+        if(finish)
+        {
+            try {
+                GameMidlet.gameCanvas.setGameScene(new MapScene());
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public void pointerPressed(int x, int y) {
+        System.out.println(x + "" + y + "");
+    }
+
+    public void pointerReleased(int x, int y) {
+        System.out.println(x + "" + y + "");
+    }
+
+    public void pointerDragged(int x, int y) {
+        System.out.println(x + "" + y + "");
+    }
+
+    public void clear(Graphics g) {
+        g.setColor(255, 255, 255);
+        g.fillRect(0, 0, 240, 320);
+        hasRenderBackground = false;
+    }
+
+    private void renderBackground(int fpr, Graphics g) {
+        clear(g);
+        g.drawImage(backgroundImage.getImage(), 0, BACKGROUND_POSA, Graphics.LEFT | Graphics.TOP);
+        g.drawImage(backgroundImage.getImage(), 0, BACKGROUND_POSB, Graphics.LEFT | Graphics.TOP);
+        System.out.println(BACKGROUND_POSA+" hahaha");
+        BACKGROUND_POSA -= fpr;
+        BACKGROUND_POSB -= fpr;
+        hasRenderBackground = true;
+    }
+}
---
+++ SackRace/src/flipbox/sackrace/gamescreen/GameRumah.java
@@ -0,0 +1,290 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flipbox.sackrace.gamescreen;
+
+import flipbox.sackrace.game.GameMidlet;
+import flipbox.sackrace.game.IGameScene;
+import flipbox.sackrace.level.LevelGenerator;
+import flipbox.sackrace.nongamescreen.StartMenuScene;
+import flipbox.sackrace.object.Player;
+import flipbox.sackrace.staticvalue.StaticData;
+import flipbox.sackrace.staticvalue.TypeList;
+import flipbox.sackrace.ui.AnimatedSprite;
+import flipbox.sackrace.ui.ButtonImageItem;
+import flipbox.sackrace.ui.ImageItem;
+import java.io.IOException;
+import javax.microedition.lcdui.Font;
+import javax.microedition.lcdui.Graphics;
+import javax.microedition.lcdui.Image;
+import javax.microedition.lcdui.game.Sprite;
+
+/**
+ *
+ * @author Yulistiyan Wardhana
+ */
+public class GameRumah implements IGameScene {
+
+    private Player player;
+    ImageItem backgroundImage, buttonCoin, buttonLife1, buttonLife2, buttonLife3;
+    ButtonImageItem buttonSlide;
+    //AnimatedSprite sprite;
+    boolean start;
+    private boolean hasInit;
+    boolean soundOn;
+    private GameMidlet midlet;
+    private static long timeLapsed = 0;
+    private boolean hasRenderBackground;
+    private static int BACKGROUND_POSA;
+    private static int BACKGROUND_POSB;
+    private static int BACKGROUND_POSC;
+
+    public void setGameMidlet(GameMidlet midelet) {
+        midlet = midelet;
+    }
+
+    public void initResource() throws IOException {
+        initPlayer();
+        initBackground();
+        initButton();
+        initLevel();
+        hasInit = true;
+        start = true;
+    }
+
+    public void render(Graphics g) {
+        //Tidak bisa dipanggil langsung apabila belum diinisialisasi
+        if (!hasInit) {
+            return;
+        }
+        //Merender background jika belum dilakukan pada perenderan sekarang
+        if (!hasRenderBackground) {
+            renderBackground(3, g);
+            g.drawImage(buttonCoin.getImage(), buttonCoin.getX(),
+                    buttonCoin.getY(), Graphics.RIGHT | Graphics.TOP);
+        }
+        //Mulai menjalankan algoritma permainan
+        if (start) {
+            try {
+                //Generate rintangan
+                LevelGenerator.run(g);
+
+                //Peletakkan gambar nyawa yang dimiliki di layar
+                renderLife(g);
+                //Akhir dari peletakkan gambar nyawa
+
+                //Peletakkan Score yang didapat
+                renderScore(g);
+                //Akhir dari peletakkan Score
+
+                //Algoritma jika tombol slide ditekan dan animasi slide
+                //sudah mencapai akhir
+                if (buttonSlide.isOnPressed()) {
+                    if (player.getSprite().getFrame() == player.getSprite().
+                            getFrameSequenceLength() - 1) {
+                        System.out.println(player.getSprite().getFrame());
+                        player.getSprite().setFrame(player.getSprite().
+                                getFrameSequenceLength() - 1);
+                    } else {
+                        player.getSprite().update(timeLapsed);
+                        timeLapsed++;
+                    }
+                } else {
+                    //Algoritma jika karakter berjalan normal
+                    player.getSprite().update(timeLapsed);
+                    timeLapsed++;
+                }
+
+                //menggambar karakter ke layar
+                player.getSprite().paint(g);
+
+                //menggambar tombol slide ke layar
+                if (buttonSlide.isVisible()) {
+                    g.drawImage(buttonSlide.getImage(), buttonSlide.getX(),
+                            buttonSlide.getY(), Graphics.TOP | Graphics.LEFT);
+                }
+                //Akhir dari peletakan item di layar, flag penggambaran background 
+                //selanjutnya diset false lagi
+                hasRenderBackground = false;
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public void pointerPressed(int x, int y) {
+        if (buttonSlide.isCanClick()
+                && x >= buttonSlide.getX() && x <= (buttonSlide.getX() + buttonSlide.getWidth())
+                && y >= buttonSlide.getY() && y <= (buttonSlide.getY() + buttonSlide.getHeight())) {
+            buttonSlide.setOnPressed(true);
+            try {
+                setNgesot();//GameMidlet.gameCanvas.setGameScene(new StartMenuScene());
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public void pointerReleased(int x, int y) {
+        if (buttonSlide.isOnPressed()
+                && x >= buttonSlide.getX() && x <= (buttonSlide.getX() + buttonSlide.getWidth())
+                && y >= buttonSlide.getY() && y <= (buttonSlide.getY() + buttonSlide.getHeight())) {
+            //resetButton();
+            try {
+                setNormal();
+                buttonSlide.setOnPressed(false);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public void pointerDragged(int x, int y) {
+        //throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void clear(Graphics g) {
+        g.setColor(255, 255, 255);
+        g.fillRect(0, 0, 240, 320);
+        hasRenderBackground = false;
+    }
+
+    /*
+     * Metode untuk menginisialisasi rintangan sesuai levelnya
+     */
+    private void initLevel() {
+        LevelGenerator.initConstraints(6, 10, 0, 0, 1, 2, 30, 50);
+        LevelGenerator.initDistance(100, 300, 180, 200);
+        LevelGenerator.initObjective(TypeList.DISTANCE, 1000);
+        LevelGenerator.generateObstacles();
+        LevelGenerator.generateCoins();
+    }
+
+    /*
+     * Metode untuk menginisialisasi karaktr permainan
+     */
+    private void initPlayer() {
+        try {
+            player = new Player();
+            Image bagong = StaticData.rotateImage(Image.createImage(
+                    "/resource/chars/bagong_lompat.png"), 90);
+            player.setSprite(new AnimatedSprite(bagong, 76, 55, 3));
+            player.setJumpHeight(10);
+            player.setName("Bagong");
+            player.setBloodLevel(5);
+            player.getSprite().setPosition(17, 150);
+            player.getSprite().play();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /*
+     * Metode untuk menginisialisasi background
+     */
+    private void initBackground() throws IOException {
+        backgroundImage = new ImageItem("/resource/rumah/Sack Runner-01.jpg");
+        resetBackgroundPos();
+    }
+
+    /*
+     * Metode untuk mengembalikan posisi background di layar sesuai dengan
+     * tampilan awal permainan
+     */
+    private void resetBackgroundPos() {
+        BACKGROUND_POSA = 0;
+        BACKGROUND_POSB = backgroundImage.getWidth() - 40;
+        BACKGROUND_POSC = 2 * BACKGROUND_POSB;
+    }
+
+    /*
+     * Metode untuk menginisialisasi tombol
+     */
+    private void initButton() throws IOException {
+        buttonSlide = new ButtonImageItem("/resource/button/slide.png",
+                "/resource/button/slide.png");
+        buttonCoin = new ImageItem("/resource/button/coin.png");
+        buttonLife1 = new ImageItem("/resource/button/heart.png");
+        buttonLife2 = new ImageItem("/resource/button/heart.png");
+        buttonLife3 = new ImageItem("/resource/button/heart.png");
+        buttonCoin.setX(230).setY(10 + 230);
+        buttonLife1.setX(230).setY(10);
+        buttonLife2.setX(230).setY(10 + buttonLife2.getWidth() + 10);
+        buttonLife3.setX(230).setY(10 + 2 * buttonLife3.getWidth() + 2 * 10);
+    }
+
+    /*
+     * Metode yang dipanggil ketika tombol slide ditekan
+     */
+    private void setNgesot() throws Exception {
+        Image bagong = StaticData.rotateImage(Image.createImage(
+                "/resource/chars/bagong_ngesot.png"), 90);
+        player.setSprite(new AnimatedSprite(bagong, 76, bagong.getHeight() / 4, 4));
+        player.getSprite().setPosition(17, 150);
+    }
+
+    /*
+     * Metode untuk menormalisasi tampilan karakter
+     */
+    private void setNormal() throws Exception {
+        Image bagong = StaticData.rotateImage(Image.createImage(
+                "/resource/chars/bagong_lompat.png"), 90);
+        player.setSprite(new AnimatedSprite(bagong, 76, 55, 3));
+        player.getSprite().setPosition(17, 150);
+        //resetButton();
+    }
+
+    /*
+     * Metode untuk mengembalikan semua tombol ke mode tidak ditekan
+     */
+    private void resetButton() {
+        buttonSlide.setOnPressed(false);
+    }
+
+    /*
+     * Metode untuk merender background sesuai dengan kecepatan penamlilan
+     *
+     * @param fpr kecepatan penamlilan @param g grafik yang akan digambar
+     */
+    private void renderBackground(int fpr, Graphics g) {
+        clear(g);
+        g.drawImage(backgroundImage.getImage(), 0, BACKGROUND_POSA, Graphics.LEFT | Graphics.TOP);
+        g.drawImage(backgroundImage.getImage(), 0, BACKGROUND_POSB, Graphics.LEFT | Graphics.TOP);
+        g.drawImage(backgroundImage.getImage(), 0, BACKGROUND_POSC, Graphics.LEFT | Graphics.TOP);
+        //System.out.println(BACKGROUND_POSA+" hahaha");
+
+        //Algoritma enyesuaian letak background di layar
+        if (BACKGROUND_POSB <= 0) {
+            resetBackgroundPos();
+        }
+        BACKGROUND_POSA -= fpr;
+        BACKGROUND_POSB -= fpr;
+        BACKGROUND_POSC -= fpr;
+        hasRenderBackground = true;
+    }
+
+    /*
+     * Metode untuk menggambar nyawa di layar
+     */
+    private void renderLife(Graphics g) {
+        g.drawImage(buttonLife1.getImage(), buttonLife1.getX(),
+                buttonLife1.getY(), Graphics.RIGHT | Graphics.TOP);
+        g.drawImage(buttonLife2.getImage(), buttonLife2.getX(),
+                buttonLife2.getY(), Graphics.RIGHT | Graphics.TOP);
+        g.drawImage(buttonLife3.getImage(), buttonLife3.getX(),
+                buttonLife3.getY(), Graphics.RIGHT | Graphics.TOP);
+    }
+
+    /*
+     * Metode untuk menggambar Score di layar
+     */
+    private void renderScore(Graphics g) throws Exception {
+        Image mutableImage = Image.createImage(20, 20);
+        Graphics grImage = mutableImage.getGraphics();
+        grImage.drawString("00", 0, 0, Graphics.LEFT | Graphics.TOP);
+        g.drawImage(StaticData.rotateImage(mutableImage, 90),
+                buttonCoin.getX() - 15, buttonCoin.getY() + 50,
+                Graphics.RIGHT | Graphics.TOP);
+    }
+}
